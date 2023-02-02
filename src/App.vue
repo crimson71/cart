b2e37ad87b1758baa966550e96f91bd2091f8264
crimson <crimson71@foxmail.com>
2023-02-02 17:53:37 +0800

    all complete

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,105 @@
 <template>
-  <div class="app-container">
-    <h1>App 根组件</h1>
+  <div class="div-container">
+    <Header title="购物车"></Header>
+    
+    <!-- 渲染数据时一定要加v-bind，不然的话只是传递一个字符串item.goods_name -->
+    <Goods
+      v-for="item in list"
+      :key="item.id"
+      :id="item.id"
+      :title="item.goods_name"
+      :price="item.goods_price"
+      :pic="item.goods_img"
+      :state="item.goods_state"
+      :count="item.goods_count"
+      @state-change="getNewState"
+    ></Goods>
+    <!-- 代码规范先指令，再绑定，最后是事件 -->
+    <Footer :isfull="fullState" :amount="amt" :all="total" @full-change="getFullState" ></Footer>
+    
   </div>
 </template>
 
 <script>
-export default {}
+import axios from "axios";
+// 导入组件
+import Header from "@/components/Header/Header.vue";
+import Goods from "@/components/Goods/Goods.vue";
+import Footer from "@/components/Footer/Footer.vue";
+import bus from '@/components/eventBus.js'
+
+export default {
+  components: {
+    Header,
+    Goods,
+    Footer,
+    
+  },
+  data() {
+    return {
+      list: [],
+    };
+  },
+  methods: {
+    async initCartList() {
+      const { data: res } = await axios.get("https://www.escook.cn/api/cart");
+      if (res.status === 200) {
+        this.list = res.list;
+        console.log(this.list);
+      }
+    },
+    getNewState(e) {
+      this.list.some((item) => {
+        if (item.id === e.id) {
+          item.goods_state = e.value;
+          return true;
+        }
+      });
+    },
+    getFullState(val) {
+      this.list.forEach(item => {
+        
+        item.goods_state = val
+
+      })
+    }
+  },
+  created() {
+    this.initCartList();
+    bus.$on('share',(val) => {
+      this.list.some(item => {
+        if(item.id === val.id) {
+          item.goods_count = val.value
+          return true
+        }
+      })
+
+    })
+  },
+  computed: {
+    fullState() {
+      return this.list.every((item) => item.goods_state);
+    },
+    amt() {
+      return this.list
+      .filter(item => item.goods_state)
+      .reduce((total,item) => (
+        total += item.goods_price * item.goods_count
+      ),0)
+
+    },
+    total() {
+      return this.list.filter(item => item.goods_state) .reduce((t,item) => 
+        t += item.goods_count
+      ,0)
+
+    }
+  },
+};
 </script>
 
 <style lang="less" scoped>
-.app-container {
+.div-container {
   padding-top: 45px;
   padding-bottom: 50px;
 }
--- src/components/Goods/Goods.vue
+++ src/components/Goods/Goods.vue
@@ -4,28 +4,80 @@
     <div class="thumb">
       <div class="custom-control custom-checkbox">
         <!-- 复选框 -->
-        <input type="checkbox" class="custom-control-input" id="cb1" :checked="true" />
-        <label class="custom-control-label" for="cb1">
+        <input type="checkbox" class="custom-control-input" :id="'cb' + id" :checked="state" @change="stateChange" />
+        <label class="custom-control-label" :for="'cb' + id">
           <!-- 商品的缩略图 -->
-          <img src="../../assets/logo.png" alt="" />
+          <img :src="pic" alt="" />
         </label>
       </div>
     </div>
     <!-- 右侧信息区域 -->
     <div class="goods-info">
       <!-- 商品标题 -->
-      <h6 class="goods-title">商品名称商品名称商品名称商品名称</h6>
+      <h6 class="goods-title">{{ title }}</h6>
       <div class="goods-info-bottom">
         <!-- 商品价格 -->
-        <span class="goods-price">￥0</span>
+        <span class="goods-price">￥{{ price }}</span>
         <!-- 商品的数量 -->
+        <Counter :num="count" :id="id"></Counter>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+import Counter from '@/components/Counter/Counter.vue'
+import { type } from 'os';
+
+export default {
+  // props属性的数据为只读，用v-bind
+  props: {
+    // 商品的 id
+    // 为啥在这里要封装一个 id 属性呢？
+    // 原因:将来,子组件中商品的勾选状态变化之后, 需要通过子 -> 父的形式,
+    // 通知父组件根据 id 修改对应商品的勾选状态。
+    id: {
+      required:true,
+      type:Number,
+    },
+    title:{ 
+      default:'',
+      type:String,
+
+    },
+    price: {
+      dafault:0,
+      type:Number,
+
+    },
+    pic: {
+      default:'',
+      type:String,
+     
+    },
+    state: {
+      default:true,
+      type:Boolean,
+    },
+    count: {
+      default:1,
+      type:Number,
+    }
+  },
+  methods: {
+    // 只要复选框的选中状态发生了变化，就会调用这个处理函数
+    // 向父组件传递数据用自定义事件$emit('事件名'，要向父组件传递的数据)
+    stateChange(e) {
+      const newState = e.target.checked
+      this.$emit('state-change',{ id:this.id,value:newState})
+
+    }
+    
+  },
+  components: {
+    Counter
+  }
+}
 </script>
 
 <style lang="less" scoped>
